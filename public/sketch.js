f7db21cd6c77d5b468f5c0668f352832a387c8af
Francesco <francesco.invidia.98@gmail.com>
2020-01-21 16:04:31 +0100

    first

---
+++ public/sketch.js
@@ -0,0 +1,17 @@
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth*3,windowHeight*4)
+  background(0,0,0)
+  strokeWeight(5)
+  stroke(255)
+  line(0,0,width,height)
+  line(0,width,height,0)
+
+}
+
+function draw() {
+  // put drawing code here
+}
